19f4c59d15aa24b2872b652dd501bfe37ffc7ed5
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-06-27 14:29:03 -0700

    Added commentary to the code

--- js/AboutUs.js
+++ js/AboutUs.js
@@ -1,22 +1,28 @@
+//Use of DOM and Components to allow us to filter betweent he different team members.
 class TabLink {
     constructor(tabElement) {
       this.tabElement = tabElement;
+      //Found the data set associated with each tab.
       this.tabData = tabElement.dataset.tab;
+      
   
-
+      //Used the below statement to identify what tabdata is being used so we know what content to showcase.
       if (this.tabData == "all") {
         this.person = document.querySelectorAll(".person");
       } else {
         this.person = document.querySelectorAll(`.person[data-tab='${this.tabData}']`);
       }
-  
+      
+      //Created a new array which we turn into a class after filtering with ".map" . We are going through the data for person/s being pulled from the if stated.
       this.person = Array.from(this.person).map((person) => {
         return new PersonBio(person)
       });
-  
+      
+      //this is where we are using dom to help us respond to the users interaction on the webpage.
       this.tabElement.addEventListener("click", () => this.selectTab());
     }
   
+    //method that is being called in the dom. It is deteremining how we respond when we click on one of the tabs. It also calls in the method from the "child" class we created above.
     selectTab() {
   
       const tabs = document.querySelectorAll('.tab');
@@ -32,16 +38,19 @@ class TabLink {
       })
 
       this.tabElement.classList.add("active-tab");
-  
+      
+      //this is where we are calling the method from class PerosnBio to access the information we need (the person)
       this.person.forEach(person => person.selectCard());
     }
   }
   
+  //class created through TabLink
   class PersonBio {
     constructor(person) {
       this.person = person;
   
     }
+    //This is allowing us to see all the content that comes with the person/s that are associated with the tab we clicked on.
     selectCard() {
       this.person.style.display = "flex";
     }
@@ -49,6 +58,7 @@ class TabLink {
   }
 
   let tabs = document.querySelectorAll(".tab");
+  //Grabbing the different roles of the team. 
   
   tabs.forEach((element) => {
     return new TabLink(element);
--- js/Index.js
+++ js/Index.js
@@ -1,19 +1,24 @@
 class Question{
     constructor(QandA){
-        console.log(QandA)
         this.QandA = QandA;
         this.open = QandA.querySelector(".open");
+        //We filled in the text here. This was moreso to see how this works and better understand what we did in the previous lessons in class.
         this.open.textContent = "Answer"
 
+        //again use of dom here to expan the content when user press on "Answer"
         this.open.addEventListener('click', ()=> this.openQuestion())
     }
+
+    //the method that is being called on in addEventListener that allows us to toggle "Answer" by clicking on it.
     openQuestion(){
         this.QandA.classList.toggle("question-open");
     }
 }
 
+//Created a constant for all the questions being asked
 const questions = document.querySelectorAll('.question');
 
+//Created a new class for questions.
 questions.forEach(function(QandA){
     return new Question(QandA)
 });
